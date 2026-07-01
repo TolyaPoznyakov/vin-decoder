2ed4cfab24568710f8ae72dae66b4af5236daa65
TolyaPoznyakov <poznjakov373@gmail.com>
2026-07-01 17:56:52 +0300

    update vehicle history

--- src/hooks/useVinDecoder.ts
+++ src/hooks/useVinDecoder.ts
@@ -2,27 +2,21 @@ import { useState } from "react";
 import type { Vehicle } from "../types/vehicle";
 import { decodeVin } from "../plugins/vinService.ts";
 import { mapVehicle } from "../utils/mapVehicle";
-import {useLocalStorage} from "./useLocalStorage.ts";
+import { useLocalStorage } from "./useLocalStorage.ts";
 
 export function useVinDecoder() {
-
   const [data, setData] = useLocalStorage<Vehicle | null>(
     "currentVehicle",
-    null
+    null,
   );
 
-  const [history, setHistory] = useLocalStorage<Vehicle[]>(
-    "history",
-    []
-  );
+  const [history, setHistory] = useLocalStorage<Vehicle[]>("history", []);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [message, setMessage] = useState("");
 
-
   const decode = async (vin: string) => {
-
     if (data?.vin === vin) {
       setMessage("This VIN is already loaded");
       return;
@@ -54,28 +48,28 @@ export function useVinDecoder() {
         setMessage(response.Message);
       }
 
+      if (data) {
+        setHistory((prev) => {
+          const deduped = [data, ...prev.filter((v) => v.vin !== data.vin)];
+          return deduped.slice(0, 3);
+        });
+      }
+
       setData(vehicle);
-
     } catch (error: unknown) {
-
       if (error instanceof Error) {
         setError(error.message);
       } else {
         setError("Failed to decode VIN");
       }
-
     } finally {
       setLoading(false);
     }
   };
 
-
   const deleteFromHistory = (vin: string) => {
-    setHistory((prev) =>
-      prev.filter((v) => v.vin !== vin)
-    );
+    setHistory((prev) => prev.filter((v) => v.vin !== vin));
   };
-
 
   return {
     data,
